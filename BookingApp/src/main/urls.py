93f2d445a2a8b296a0fa953b91d72530a4a6ab9b
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2023-05-25 11:37:30 +0200

    Added the url file to the main app

--- BookingApp/src/main/urls.py
+++ BookingApp/src/main/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic import RedirectView
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include("main.urls"))
+    path("index/",views.index,name = 'index'),
+
+]
+urlpatterns += [
+    path('', RedirectView.as_view(url='index/', permanent=True)),
 ]
