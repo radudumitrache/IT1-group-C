ad92cae0584c5d371a09b7d2f99fc40645fc97c2
Alex Schuepbach <alex.schuepbach@student.nhlstenden.com>
2023-06-12 16:09:40 +0200

    Scrolling works with multiple lines

--- hardware functions/textscroll.py
+++ hardware functions/textscroll.py
@@ -1,38 +1,78 @@
 import time
 
-name = "Dumitrache, Marian"
+names = ["Dumitrache Marian", "Schuepbach Alex", "Nefeli Chrysostomou"]
 #availableSpace could just be a constant and not be passed into the function
 availableSpace = 15
 
-#this function is not usable for multiple pieces of text, concurrent processing is currently not possible
-def displayText(text: str, screenLength: int):
 
-    if len(text) > screenLength:
+def displayText(texts: list, screenLength: int):
 
-        index = 0
-        while index + screenLength <= len(text):
+    counter = 0
+    index = 0
+    index1 = 0
+    index2 = 0
+    index3 = 0
 
-            # determines where to start displaying the string from
-            charIndex = 0 + index
-            stringBuild = ""
+    while True:
 
-            while (charIndex < screenLength + index) & (charIndex < len(text)):
-                #add current string character to the stringBuild, this could be done with substrings
-                stringBuild = stringBuild + text[charIndex]
+        for text in texts:
 
-                #next character
-                charIndex += 1
+            match counter:
+                case 0:
+                    index = index1
+                case 1:
+                    index = index2
+                case 2:
+                    index = index3
 
-            # replace with screen output command
-            print(stringBuild)
-            index += 1
-            time.sleep(0.5)
+            if len(text) > screenLength:
+                print(index + screenLength)
+                print(len(text))
+                if index + screenLength <= len(text):
 
-        # delay, modify as necessary
-        time.sleep(1)
+                    # determines where to start displaying the string from
+                    charIndex = 0 + index
+                    stringBuild = ""
 
-    else:
-        print(text)
+                    while (charIndex < screenLength + index) & (charIndex < len(text)):
+                        #add current string character to the stringBuild, this could be done with substrings
+                        stringBuild = stringBuild + text[charIndex]
 
-displayText(name, availableSpace)
-displayText(name, availableSpace)
+                        #next character
+                        charIndex += 1
+
+                    # replace with screen output command
+                    print(stringBuild)
+                    index = index + 1
+                    match counter:
+                        case 0:
+                            index1 = index
+                        case 1:
+                            index2 = index
+                        case 2:
+                            index3 = index
+                    time.sleep(0.5)
+
+                else:
+                    match counter:
+                        case 0:
+                            index1 = 0
+                        case 1:
+                            index2 = 0
+                        case 2:
+                            index3 = 0
+
+                # delay, modify as necessary
+                time.sleep(1)
+
+            else:
+                print(text)
+                time.sleep(0.5)
+
+            if counter >= 2:
+                counter = 0
+            else:
+                counter = counter + 1
+        print()
+
+displayText(names, availableSpace)
